d73883c2cd2170171d368084360f918720411833
mwisnie <>
2018-06-26 20:25:43 +0200

    basic project setup with Spring Initlzr

---
+++ src/main/java/mwisnie/project/airqualitymonitorbackend/AirQualityMonitorBackendApplication.java
@@ -0,0 +1,12 @@
+package mwisnie.project.airqualitymonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityMonitorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirQualityMonitorBackendApplication.class, args);
+	}
+}
