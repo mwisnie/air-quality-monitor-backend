00dae163abd1f3c39aa59eb6ca16589754454a5a
mwisnie <>
2018-06-27 21:03:03 +0200

    MongoDB connection with Spring Data, adjusted repository, dependencies

--- src/main/java/mwisnie/project/airqualitymonitorbackend/entity/User.java
+++ src/main/java/mwisnie/project/airqualitymonitorbackend/entity/User.java
@@ -3,17 +3,12 @@ package mwisnie.project.airqualitymonitorbackend.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
-@Entity
 @Getter
 @Setter
 public class User {
 
-    @Id
-    @GeneratedValue
+
     private Long id;
 
     private String username;
--- src/main/java/mwisnie/project/airqualitymonitorbackend/repository/UserRepository.java
+++ src/main/java/mwisnie/project/airqualitymonitorbackend/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package mwisnie.project.airqualitymonitorbackend.repository;
 
+
 import mwisnie.project.airqualitymonitorbackend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, Long> {
+
 }
